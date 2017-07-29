201ef0f9c42464a9f16f8ea4a60d4d44dafaf2b5
mdeiml <matthias@deiml.net>
2017-07-29 08:31:44 +0200

    Init libgdx

---
+++ desktop/src/com/mdeiml/ld39/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mdeiml.ld39.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mdeiml.ld39.LD39Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD39Game(), config);
+	}
+}
